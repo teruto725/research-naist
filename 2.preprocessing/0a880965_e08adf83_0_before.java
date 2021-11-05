4a132692b5029d7867126d7845c15df62227e356
= <=>
2021-11-05 14:44:04 +0900

    update  for meeting

---
+++ 2.preprocessing/0a880965_e08adf83_0_before.java
@@ -0,0 +1,7 @@
+public AuthenticatorSelectionDialogBridge(long nativeAuthenticatorSelectionDialogView,
+        Context context, ModalDialogManager modalDialogManager) {
+    mNativeCardUnmaskAuthenticationSelectionDialogView = nativeAuthenticatorSelectionDialogView;
+    mContext = context;
+    mAuthenticatorSelectionDialog =
+            new AuthenticatorSelectionDialog(context, this, modalDialogManager);
+}
